10778de9fdf83419d3c3369f6b2621f72eb8f3d3
Adam Henley <adam@adamhenley.com>
2016-07-01 23:23:02 +1200

    initial commit

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,32 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-estimote/plugin/src/js/EstimoteBeacons.js",
+        "id": "cordova-plugin-estimote.EstimoteBeacons",
+        "clobbers": [
+            "estimote"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-estimote": "0.8.0",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-geolocation": "2.2.0"
+};
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,21 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-estimote/plugin/src/js/EstimoteBeacons.js",
+        "id": "cordova-plugin-estimote.EstimoteBeacons",
+        "pluginId": "cordova-plugin-estimote",
+        "clobbers": [
+            "estimote"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-estimote": "0.8.0",
+    "cordova-plugin-compat": "1.0.0",
+    "cordova-plugin-geolocation": "2.2.0"
+}
+// BOTTOM OF METADATA
+});
