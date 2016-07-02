323e0806c0a8b80990cea87becc605adc1bddb46
Adam Henley <adam@adamhenley.com>
2016-07-02 20:43:24 +1200

    blue

---
+++ platforms/browser/www/js/fix-data.js
@@ -0,0 +1,23 @@
+var fixes =
+{
+  "0":
+  {
+    "packname": "Pack 1",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus mauris et leo tincidunt finibus. Nam ac lacinia turpis. Integer rutrum nibh non sapien interdum varius. Fusce consectetur dolor eu tincidunt consequat. ",
+    "fomos": [0,1,2]
+  },
+  "1":
+  {
+    "packname": "Pack 2",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus mauris et leo tincidunt finibus. Nam ac lacinia turpis. Integer rutrum nibh non sapien interdum varius. Fusce consectetur dolor eu tincidunt consequat. ",
+    "fomos": [3,4,5]
+  },
+  "2":
+  {
+    "id": 2,
+    "packname": "Pack 3",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi cursus mauris et leo tincidunt finibus. Nam ac lacinia turpis. Integer rutrum nibh non sapien interdum varius. Fusce consectetur dolor eu tincidunt consequat. ",
+    "fomos": [1,3,4,2]
+  }
+
+};
---
+++ platforms/browser/www/js/fomo-data.js
@@ -0,0 +1,59 @@
+var fomos = {
+  "0":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+  },
+  "1":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+
+  },
+  "2":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+
+  },
+  "3":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+
+  },
+  "4":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+
+  },
+  "5":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+
+  },
+  "6":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+
+  },
+  "7":
+  {
+    "fomoname": "",
+    "description": "",
+    "points": 20
+
+  }
+
+
+};
